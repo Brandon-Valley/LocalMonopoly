3b34cccf5be843ece6e84968bab066db25b0d78d
Brandon-Valley <22589462+Brandon-Valley@users.noreply.github.com>
2026-06-26 14:53:16 +0000

    Update header style link

--- scripts/link-button-fix-css.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const p = 'index.html';
-let s = fs.readFileSync(p, 'utf8');
-const link = '<link rel="stylesheet" href="github-button-fix.css?v=2">';
-if (!s.includes('github-button-fix.css')) {
-  s = s.replace('</head>', link + '\n</head>');
-}
-fs.writeFileSync(p, s, 'utf8');
